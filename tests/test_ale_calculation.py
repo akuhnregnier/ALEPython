Returned ALEs at each edge, not at centres. Fixed nearest neighbour filling algorithm. Multiple values are now also averaged. Unified the API by consistently returning quantiles first, followed by the ALE values and others if needed. The plotting functions then take these outputs as inputs in the same order. Fixed plotting of quantiles for quantile-based axes. Returned dictionaries of axes from all plotting functions in order to be able to return multiple axes. Used f-strings, meaning the minimum Python version is now 3.6. Used tqdm auto to enable ipywidgets-based progress meters in Jupyter notebooks. _get_centres is now able to handle arbitrary axis arguments (and potentially inplace). Updated docstrings. Made core plotting and calculation methods public by removing the leading underscores. Added more error handling, masking and interpolation options to the 2D ALE plotting function, along with multiple plotting modes ('contourf', 'grid', and 'gridcontour'). Enabled visualisation of combined effects by adding lower order effects to ALE plots. Calculated the 2D ALE mask based on neighbouring quantile validity, instead of just empty cells. Allowed the usage of existing figures and axes in all plotting functions. Updated and added tests.

tests/test_ale_calculation.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-
 import numpy as np
 import pandas as pd
 
-from alepython.ale import _first_order_ale_quant, _get_centres, _second_order_ale_quant
+from alepython.ale import first_order_ale_quant, second_order_ale_quant
 
 from .utils import interaction_predictor, linear_predictor
 
@@ -25,16 +24,13 @@ def test_linear():
 
     # Test that the first order relationships are linear.
     for column in X.columns:
-        ale, quantiles = _first_order_ale_quant(linear_predictor, X, column, 21)
-        centres = _get_centres(quantiles)
-        p, V = np.polyfit(centres, ale, 1, cov=True)
+        quantiles, ale = first_order_ale_quant(linear_predictor, X, column, 10)
+        p, V = np.polyfit(quantiles, ale, 1, cov=True)
         assert np.all(np.isclose(p, [1, -0.5], atol=1e-3))
         assert np.all(np.isclose(np.sqrt(np.diag(V)), 0))
 
     # Test that a second order relationship does not exist.
-    ale_second_order, quantiles_list = _second_order_ale_quant(
-        linear_predictor, X, X.columns, 21
-    )
+    ale_second_order = second_order_ale_quant(linear_predictor, X, X.columns, 21)[1]
     assert np.all(np.isclose(ale_second_order, 0))
 
 
@@ -43,11 +39,27 @@ def test_interaction():
     np.random.seed(1)
 
     N = int(1e6)
-    X = pd.DataFrame({"a": np.random.random(N), "b": np.random.random(N)})
-    ale, quantiles_list = _second_order_ale_quant(
-        interaction_predictor, X, X.columns, 61
-    )
+    nbins = 3  # This needs to be an odd number to yield an even number of edges.
 
-    # XXX: There seems to be a small deviation proportional to the first axis ('a')
-    # that is preventing this from being closer to 0.
-    assert np.all(np.abs(ale[:, :30] - ale[:, 31:][::-1]) < 1e-2)
+    b = np.linspace(0, 1, N)
+    # Mirror a around b=0.5.
+    a_comp = np.random.random(N // 2) * 2
+    a = np.append(a_comp, a_comp[::-1])
+    X = pd.DataFrame({"a": a, "b": b})
+
+    quantiles_list, ale = second_order_ale_quant(
+        interaction_predictor, X, X.columns, nbins
+    )[:2]
+
+    b_quantiles = quantiles_list[1]
+    # Check that the quantiles are mirrored.
+    assert np.allclose(
+        b_quantiles[: (nbins + 1) // 2],
+        1 - np.array(b_quantiles[(nbins + 1) // 2 :][::-1]),
+    )
+    # Check that the ALE is mirrored as expected from the nature of the
+    # `interaction_predictor`. The error here (which we require to be below `atol`
+    # (roughly)) is observed to decrease with `N`, as expected.
+    assert np.allclose(
+        ale[:, : (nbins + 1) // 2], ale[:, (nbins + 1) // 2 :][::-1, ::-1], atol=1e-3
+    )

tests/utils/test_models.py
@@ -26,15 +26,19 @@ def linear_predictor(X):
 
 
 def interaction_predictor(X):
-    """Interaction changes sign at b = 0.5."""
+    """Interaction changes sign at b = 0.5.
+
+    Assumes b is uniformly distributed in [0, 1).
+
+    """
     a = X["a"]
     b = X["b"]
 
     out = np.empty_like(a)
 
-    mask = b <= 0.5
+    mask = b < 0.5
     out[mask] = a[mask] * b[mask]
     mask = ~mask
-    out[mask] = -a[mask] * b[mask]
+    out[mask] = -a[mask] * (1 - b[mask])
 
     return out