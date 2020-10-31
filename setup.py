e453b64105701b5a543a885a41a469f44acba992
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2020-10-31 18:56:15 +0000

    Fixed typo in license description.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approched :: Apache 2",
+        "License :: OSI Approved :: Apache 2",
         "Operating System :: OS Independent",
     ],
 )
